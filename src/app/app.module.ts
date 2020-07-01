412f47c5b69091668b8822b917940bb1ca9000d8
maxi <maxiranqui@gmail.com>
2020-07-01 18:44:07 -0300

    se agrega formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CrearPersonaComponent } from './crear-persona/crear-persona.component';
-import { ConsultarPersonaComponent } from './consultar-persona/consultar-persona.component';
+import { ConsultarPersonaComponent } from './consultar-persona/consultar-persona.component'; 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -16,7 +26,18 @@ import { ConsultarPersonaComponent } from './consultar-persona/consultar-persona
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatTableModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/crear-persona/crear-persona.component.ts
+++ src/app/crear-persona/crear-persona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-crear-persona',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearPersonaComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private fb: FormBuilder ) { }
+
+  personaForm = this.fb.group({
+    nombre: [''],
+    apellido: [''],
+    edad: [''],
+    direccion: ['']
+
+  })
 
   ngOnInit(): void {
   }
-
+submit(){
+  this.personaForm.value;
+}
 }
