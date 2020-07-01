1609478fc6be96ed0249ab27c8f1321b11e2f5a7
maxi <maxiranqui@gmail.com>
2020-07-01 17:53:42 -0300

    se crean componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CrearPersonaComponent } from './crear-persona/crear-persona.component';
+import { ConsultarPersonaComponent } from './consultar-persona/consultar-persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearPersonaComponent,
+    ConsultarPersonaComponent
   ],
   imports: [
     BrowserModule,
